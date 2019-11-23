b93d17bd1fe90d842e1d7c2978e6034bd33fde2f
tratatini <57325650+tratatini@users.noreply.github.com>
2019-11-23 19:19:39 +0200

    cors added

--- app.py
+++ app.py
@@ -3,14 +3,26 @@ from flask import jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 
-@app.route('/')
-def hello_world():
-    d = {'point1': [42, 10],
-         'point2': [12, 17]}
-    return jsonify(d)
+@app.route('/data')
+def get_data():
+    features = {
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point",
+                         "coordinates": [102.0, 0.5]},
+            "properties": {"name": "value0"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [102.0, 0.5]},
+            "properties": {"name": "value0", "note": "note"}
+        }
+    }
+
+    return jsonify(features)
 
 
 if __name__ == '__main__':
