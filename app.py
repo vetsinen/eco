c9ac8f27bd97e200a289a90d76785f4506775833
vetsinen <vetsinen@gmail.com>
2019-11-23 18:41:07 +0200

    cors added

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.route('/')
-def hello_world():\
-    return 'Hello World!'
-
-
-@app.route('/data')
-def data():
+def hello_world():
     d = {'point1': [42, 10],
          'point2': [12, 17]}
     return jsonify(d)
